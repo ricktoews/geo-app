4b122dcc43ee7469b9cd6bf1709de13a8f02cb2d
Rick Toews <rick.toews@gmail.com>
2023-04-30 22:51:53 -0700

    Change from checkboxes to buttons for continent selection.

--- components/ContinentSelector.js
+++ components/ContinentSelector.js
@@ -1,104 +1,89 @@
 import { useEffect, useState } from 'react';
 
+const CHECKED = `bg-purple-500`;
+const UNCHECKED = `bg-gray-500`;
 export default function CheckboxGroup({ continents, setContinentSelection }) {
-    const [checked, setChecked] = useState({
-        'north-america': false,
-        'south-america': false,
-        europe: false,
-        africa: false,
-        asia: false,
+    const [buttonColors, setButtonColors] = useState({
+        'north-america': UNCHECKED,
+        'south-america': UNCHECKED,
+        europe: UNCHECKED,
+        africa: UNCHECKED,
+        asia: UNCHECKED,
     });
 
     useEffect(() => {
         const initContinents = {};
         for (let continent of continents) {
-            initContinents[continent] = true;
+            initContinents[continent] = CHECKED;
         }
 
-        setChecked((prevChecked) => ({ ...prevChecked, ...initContinents }));
+        const colors = { ...buttonColors, ...initContinents };
+        setButtonColors(colors);
     }, [])
 
     const continentGroupA = ['north-america', 'south-america'];
     const continentGroupB = ['europe', 'africa', 'asia'];
-    const handleCheckboxChange = (event) => {
-        const { name, checked: isChecked } = event.target;
-        setChecked((prevChecked) => ({
-            ...prevChecked,
-            [name]: isChecked,
-            ...(isChecked && continentGroupA.indexOf(name) !== -1 && {
-                europe: false,
-                africa: false,
-                asia: false,
-            }),
-            ...(isChecked && continentGroupB.indexOf(name) !== -1 && {
-                'north-america': false,
-                'south-america': false,
-            }),
-        }));
-    };
+    function handleContinentClicked(event) {
+        const temp = JSON.parse(JSON.stringify(buttonColors));
+        const el = event.target;
+        const { continent } = el.dataset;
+        const toggleToChecked = temp[continent] === UNCHECKED;
+        console.log(`====> ${continent} checked ${toggleToChecked}`);
+
+        setButtonColors((prevColors) => (
+            {
+                ...prevColors,
+                [continent]: toggleToChecked ? CHECKED : UNCHECKED,
+                ...(toggleToChecked && continentGroupA.indexOf(continent) !== -1 && {
+                    europe: UNCHECKED,
+                    africa: UNCHECKED,
+                    asia: UNCHECKED,
+                }),
+                ...(toggleToChecked && continentGroupB.indexOf(continent) !== -1 && {
+                    'north-america': UNCHECKED,
+                    'south-america': UNCHECKED,
+                })
+            }
+        ));
+    }
+
 
     function handleSetSelection() {
-        setContinentSelection(checked);
+        console.log('====> handleSetSelection', buttonColors);
+        const selection = {};
+        for (let b in buttonColors) {
+            selection[b] = false;
+            if (buttonColors[b] === CHECKED) {
+                selection[b] = true;
+            }
+        }
+
+        setContinentSelection(selection);
     }
 
     return (
-        <div className="border border-white rounded-lg p-4 space-y-4">
-            <p className="text-white font-bold mb-2">Continent Selection</p>
-            <div className="flex space-x-4">
-                <label className="inline-flex items-center text-white">
-                    <input
-                        type="checkbox"
-                        className="form-checkbox h-5 w-5 text-white"
-                        name="north-america"
-                        checked={checked['north-america']}
-                        onChange={handleCheckboxChange}
-                    />
-                    <span className="ml-2">North America</span>
-                </label>
-                <label className="inline-flex items-center text-white">
-                    <input
-                        type="checkbox"
-                        className="form-checkbox h-5 w-5 text-white"
-                        name="south-america"
-                        checked={checked['south-america']}
-                        onChange={handleCheckboxChange}
-                    />
-                    <span className="ml-2">South America</span>
-                </label>
+        <div className="border border-white rounded-lg my-2">
+            <p className="text-white bg-gray-500 font-bold mb-2">Continent Selection</p>
+            <div className="flex justify-between px-3">
+                <div className="flex flex-col space-y-3 py-3">
+                    <div className="flex space-x-3">
+                        <div><button onClick={handleContinentClicked} data-continent="north-america" className={`whitespace-normal text-xs leading-none h-5 rounded-full px-4 py-4 ${buttonColors['north-america']} text-white flex items-center`}>North America</button></div>
+                        <div><button onClick={handleContinentClicked} data-continent="south-america" className={`whitespace-normal text-xs leading-none h-5 rounded-full px-4 py-4 ${buttonColors['south-america']} text-white flex items-center`}>South America</button></div>
+                    </div>
+                    <div className="flex space-x-3">
+                        <div><button onClick={handleContinentClicked} data-continent="europe" className={`h-5 rounded-full px-4 py-4 ${buttonColors.europe} text-white flex items-center`}>Europe</button></div>
+                        <div><button onClick={handleContinentClicked} data-continent="africa" className={`h-5 rounded-full px-4 py-4 ${buttonColors.africa} text-white flex items-center`}>Africa</button></div>
+                        <div><button onClick={handleContinentClicked} data-continent="asia" className={`h-5 rounded-full px-4 py-4 ${buttonColors.asia} text-white flex items-center`}>Asia</button></div>
+                    </div>
+                </div>
+                <svg onClick={handleSetSelection} className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                    <circle cx="15" cy="15" r="15" fill="#4CAF50" />
+                    <path d="M10.878 15.357l-2.207-2.207a1.5 1.5 0 0 1 2.121-2.121l1.086 1.086 4.293-4.293a1.5 1.5 0 0 1 2.121 2.121l-5.5 5.5a1.5 1.5 0 0 1-2.121 0z" fill="#fff" transform="translate(2 2)" />
+                </svg>
+                {/*
+                <button onClick={handleSetSelection}>Set Continent Selection</button>
+    */}
             </div>
-            <div className="flex space-x-4">
-                <label className="inline-flex items-center text-white">
-                    <input
-                        type="checkbox"
-                        className="form-checkbox h-5 w-5 text-white"
-                        name="europe"
-                        checked={checked.europe}
-                        onChange={handleCheckboxChange}
-                    />
-                    <span className="ml-2">Europe</span>
-                </label>
-                <label className="inline-flex items-center text-white">
-                    <input
-                        type="checkbox"
-                        className="form-checkbox h-5 w-5 text-white"
-                        name="africa"
-                        checked={checked.africa}
-                        onChange={handleCheckboxChange}
-                    />
-                    <span className="ml-2">Africa</span>
-                </label>
-                <label className="inline-flex items-center text-white">
-                    <input
-                        type="checkbox"
-                        className="form-checkbox h-5 w-5 text-white"
-                        name="asia"
-                        checked={checked.asia}
-                        onChange={handleCheckboxChange}
-                    />
-                    <span className="ml-2">Asia</span>
-                </label>
-            </div>
-            <button onClick={handleSetSelection}>Set Continent Selection</button>
         </div>
     );
 }
