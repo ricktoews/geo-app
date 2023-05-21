595c128cb971dc164615159b181a2b9715dbee02
Rick Toews <rick.toews@gmail.com>
2023-05-21 14:53:53 -0700

    Fix overlapping map dropdown.

--- components/DrawerItems.js
+++ components/DrawerItems.js
@@ -6,6 +6,7 @@ import CountryMap from './CountryMap';
 
 export default function DrawerItems({ children, headings = [], position = 'top' }) {
     const [drawerState, setDrawerState] = useState([false, false, false, false]);
+    const bgColors = ['bg-purple-400', 'bg-purple-500', 'bg-purple-600'];
 
     const drawers = children.map(child => child.props.header);
 
@@ -18,18 +19,18 @@ export default function DrawerItems({ children, headings = [], position = 'top' 
         setDrawerState(_drawerState);
     }
 
-    return (<div id="drawerWrapper">
+    return (<div id="drawerWrapper" className="relative z-0">
         {/* Draw drawers */}
-        <div className="z-10 flex justify-evenly w-full fixed top-0 left-0">
+        <div className="z-20 flex justify-evenly w-full fixed top-0 left-0">
             {drawers.map((d, key) => {
-                const bg = 'bg-purple-' + (100 * key + 400);
-                return <div data-drawer={key} key={key} onClick={handleClick} className={`${bg} flex-1 cursor-pointer text-center p-1 text-white`}>{d}</div>
+                const bg = bgColors[key];
+                return <div data-drawer={key} key={key} onClick={handleClick} className={`${bg} flex-1 cursor-pointer text-center bg-purple- p-1 text-white`}>{d}</div>
             })}
         </div>
 
         {/* Draw drawer contents */}
         {children.map((child, ndx) => {
-            return (<div key={ndx} className={`${drawerState[ndx] ? 'translate-y-0' : '-translate-y-60'} h-60 w-full fixed top-8 left-0 duration-500 bg-slate-600 border border-white`}>
+            return (<div key={ndx} className={`${drawerState[ndx] ? 'translate-y-0' : '-translate-y-60'} z-0 h-60 w-full fixed top-8 left-0 duration-500 bg-slate-600 border border-white`}>
                 {child}
             </div>)
         })}
